89c633e3ffdef2a66d429cad873b158da69c3aa3
Scalante <rubendarioscalante@gmail.com>
2023-02-10 14:45:25 -0500

    Se inicia el proyecto y se agregan los nugets correspondientes.

---
+++ BackEnd/Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
